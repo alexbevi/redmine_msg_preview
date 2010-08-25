fc27f9af9a6adc134127d9b30ef821c9ff68336d
Alex Bevilacqua <alexbevi@gmail.com>
2010-08-25 15:18:51 -0400

    got the call hook working properly

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_screenshot_paste do
+Redmine::Plugin.register :redmine_msg_preview do
   name 'Outlook MSG Preview'
   author 'Alex Bevilacqua'
   description 'Allow previewing of Microsoft Outlook .msg file attachments'
   version '0.0.1'
+end
+
+class RedmineScreenshotPasteHook < Redmine::Hook::ViewListener
+  render_on :view_issues_show_description_bottom, :partial => 'msg_preview'
 end
