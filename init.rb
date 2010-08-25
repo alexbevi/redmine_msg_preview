d3919b7e0f2564f52e61753b7200933350324d44
Alex Bevilacqua <alexbevi@gmail.com>
2010-08-25 15:03:20 -0400

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_screenshot_paste do
+  name 'Outlook MSG Preview'
+  author 'Alex Bevilacqua'
+  description 'Allow previewing of Microsoft Outlook .msg file attachments'
+  version '0.0.1'
+end
---
+++ lib/ole/base.rb
@@ -0,0 +1,9 @@
+# encoding: ASCII-8BIT
+
+
+require 'ole/support'
+
+module Ole # :nodoc:
+	Log = Logger.new_with_callstack
+end
+
