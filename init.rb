63d035f635bbd8e6d9bc6b09dfa766e01f49c69a
Alex Bevilacqua <alexbevi@gmail.com>
2010-12-24 08:58:34 -0500

    format message list to be just a list (not all details)

--- init.rb
+++ init.rb
@@ -8,5 +8,19 @@ Redmine::Plugin.register :redmine_msg_preview do
 end
 
 class RedmineMSGPreviewHook < Redmine::Hook::ViewListener
-  render_on :view_issues_show_description_bottom, :partial => 'msg_preview'
+  render_on :view_issues_show_description_bottom, :partial => 'msg_list'
 end
+
+module MsgHelper
+  module PublicMethods
+    def sender_to_s(msg)
+      sender_name =  msg.properties[0x3FF8]
+      sender_email = msg.properties[0x0C1F]
+
+      "#{sender_name.titlecase} [#{sender_email.downcase}]"
+    end
+  end
+end
+
+ActionView::Base.send :include, MsgHelper::PublicMethods
+
