367efc47c91e9429203c80e086cfc0bb64156171
Alex Bevilacqua <alexbevi@gmail.com>
2011-01-19 10:11:10 -0500

    sandbox the message body in an iframe when viewing (prevents the page getting all wonky due to style conflicts)
    refactored a bit as well

--- app/controllers/msg_controller.rb
+++ app/controllers/msg_controller.rb
@@ -1,12 +1,26 @@
 class MsgController < ApplicationController
   unloadable
 
+  before_filter :load_msg
+
   include FaceboxRender
 
   def show
-    file = Attachment.find(params[:file_id])
-    @msg = Mapi::Msg.open(file.diskfile)
     render_to_facebox
+  end
+  
+  def body
+    render :partial => "body", :locals => { :msg => @msg }, :layout => false
+  end
+
+###########
+# private #
+###########
+
+  def load_msg
+    @file_id = params[:file_id]
+    file = Attachment.find(@file_id)
+    @msg = Mapi::Msg.open(file.diskfile)
   end
 
 end
